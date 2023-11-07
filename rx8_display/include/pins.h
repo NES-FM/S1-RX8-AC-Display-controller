8a7f0f6bffb0fb301f61044dbd1c631104852ebd
Florian Magg <florian.magg@outlook.com>
2023-11-08 00:09:41 +0100

    Begin of complete Rewrite

---
+++ rx8_display/include/pins.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#define acAmpTX 8
+#define acAmpRX 12
+
+// PORTA Inputs
+#define matrixRow2 22
+#define matrixRow4 23
+#define matrixRow1 24
+#define matrixRow3 25
+#define rearDemistLED 26
+#define AirConLED 27
+#define reCircLED 28
+#define matrixColB 29
+
+// PORTB
+#define DATAOUT 51  // MOSI
+#define SPICLOCK 52 // sck
+#define ssPin 53    // PB0
+
+// PORTC Outputs
+#define freshAirLED 30
+#define matrixColA 31
+#define frontDemistLED 32
+#define fanInOne 33
+#define autoLED 34
+#define tempInTwo 35
+#define fanInTwo 36
+#define tempInOne 37
+
+// PORT L
+#define mode0 49
+#define mode1 48
