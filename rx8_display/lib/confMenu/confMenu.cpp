11a4d919ea486091a9fd61e29d3ce179abe65a00
Florian Magg <florian.magg@outlook.com>
2023-12-27 17:35:17 +0100

    beginning to implement a confMenu

--- rx8_display/lib/confMenu/confMenu.cpp
+++ rx8_display/lib/confMenu/confMenu.cpp
@@ -1,7 +1,44 @@
 #include "confMenu.hpp"
 
-//TODO: Implement menu short button press
-void confMenu::shortButtonPress(btn_enum shortButton) {}
+void confMenu::registerPage(confPage *item)
+{
+    if (num_el_in_conf_pages < conf_pages_size) {
+        conf_pages[num_el_in_conf_pages] = item;
+        num_el_in_conf_pages++;
+    } else {
+        logln("ERROR!!! CONF_PAGES IS NOT BIG ENOUGH");
+        delay(5000);
+    }
+}
+
+void confMenu::shortButtonPress(btn_enum shortButton) {
+    switch (shortButton)
+    {
+        case Auto:
+            enter = true;
+            break;
+        case Mode:
+            enter = true;
+            break;
+        case AC:
+            relValue++;
+            break;
+        case frontDemist:
+            relValue--;
+            break;
+        case rearDemist:
+            relValue++;
+            break;
+        case AirSource:
+            relValue--;
+            break;
+        case Off:
+            enter = true;
+            break;
+        default:
+            break;
+    }
+}
 
 //TODO: Implement menu long button press
 void confMenu::longButtonPress(btn_enum longButton) 
@@ -9,28 +46,57 @@ void confMenu::longButtonPress(btn_enum longButton)
     deactivate();
 }
 
-//TODO: Implement menu rotary switches
-void confMenu::changeRotary(int fan, int temp) {}
+void confMenu::changeRotary(int fan, int temp) {
+    relValue += fan;
+    relValue += temp;
+}
 
-//TODO: ConfMenu Activate
 void confMenu::activate() 
 {
     confMode = true;
-    // menuIndex = 1;
+    current_page = 0;
+    relValue = 0;
+    enter = false;
+    mainMenu = true;
+    midIcons.Dolby = true;
 }
 
 // TODO: ConfMenu Deactivate    
 void confMenu::deactivate() 
 {
     confMode = false;
-    // acAmpOn = true;
-    // acAmpRxChanged = true;
-    // acAmpMessageToProcess = true;
-    // menuValue = 0;
-    // menuStartValue = false;
 }
 
 void confMenu::menuTick() 
 {
+    if (mainMenu) {
+        if (relValue > 0) current_page++;
+        else if (relValue < 0) current_page--;
 
+        if (current_page == 255) current_page = num_el_in_conf_pages-1;
+        else if (current_page >= conf_pages_size) current_page = 0;
+
+        while (!conf_pages[current_page]) {
+            current_page++;
+            if (current_page == 255) current_page = num_el_in_conf_pages-1;
+            else if (current_page >= conf_pages_size) current_page = 0;
+        }
+
+        outputText = conf_pages[current_page]->menuName;
+    } else {
+        confPage* page = conf_pages[current_page];
+        page->changeValue(relValue);
+        if (enter) {
+            if (page->pressEnter()) {
+                mainMenu = true;
+                menuTick();
+                goto end;
+            }
+        }
+        outputText = page->getDisplayText();
+    }
+
+end:
+    relValue = 0;
+    enter = false;
 }
--- rx8_display/lib/confMenu/confMenu.hpp
+++ rx8_display/lib/confMenu/confMenu.hpp
@@ -1,10 +1,24 @@
 #pragma once
 #include <Arduino.h>
 
+#include "logger.h"
 #include "data_types.h"
+#include "clock.hpp"
+
+class confPage {
+    public:
+        confPage() {}
+        virtual void changeValue(int relValueChange) {}
+        virtual bool pressEnter() { return true; } // Return true if exit back to settings select
+        virtual String getDisplayText() { return ""; }
+        String menuName = "";
+    private:
+};
 
 class confMenu {
     public:
+        void registerPage(confPage* item);
+
         void activate();
         void deactivate();
 
@@ -21,4 +35,11 @@ class confMenu {
         midsectionIcons midIcons;
         String outputText;
     private:
+        const static uint8_t conf_pages_size = 10;
+        confPage* conf_pages[conf_pages_size];
+        uint8_t num_el_in_conf_pages = 0;
+        uint8_t current_page = 0;
+        int relValue = 0;
+        bool enter = false;
+        bool mainMenu = true;
 };
